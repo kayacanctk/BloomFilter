1720c14a00f7da250c727c56fc9fed0dcf6ec03a
Marynaappliedl <poberezhnaya.78@gmail.com>
2026-05-16 09:36:47 +0200

    Edited the code according to pylint regulations

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,19 +1,20 @@
+"""Bloom Filter runs performance tests across English words, DNA sequences and IDs"""
 import math
 import hashlib
-
 class BloomFilter:
     """
     Bloom Filter Data Structure Implementation.
     """
-    
+
     def __init__(self, expected_items: int, fp_rate: float):
+        """Handles data types constraints"""
         self.expected_items = expected_items
         self.fp_rate = fp_rate
-        
+
         self.size = self._get_size(expected_items, fp_rate)
-        
+
         self.hash_count = self._get_hash_count(self.size, expected_items)
-        
+
         self.bit_array = [False] * self.size
 
     def _get_hash_indices(self, item) -> list:
@@ -23,19 +24,19 @@ class BloomFilter:
         """
         indices = []
         item_str = str(item)
-        
+
         for i in range(self.hash_count):
-            seeded_item = f"{item_str}_{i}".encode('utf-8')
-            
+            seeded_item = f"{item_str}_{i}".encode("utf-8")
+
             hash_val = int(hashlib.md5(seeded_item).hexdigest(), 16)
             index = hash_val % self.size
             indices.append(index)
-            
+
         return indices
-        
+
     def add(self, item):
         """
-        Inserts an item into the Bloom Filter by setting the hashed indices to True.
+        Inserts an item into the Bloom Filter setting the hashed indices to True.
         """
         for index in self._get_hash_indices(item):
             self.bit_array[index] = True
@@ -52,7 +53,7 @@ class BloomFilter:
         return True
 
     def _get_size(self, n, p) -> int:
-        m = -(n * math.log(p)) / (math.log(2)**2)
+        m = -(n * math.log(p)) / (math.log(2) ** 2)
         return int(m)
 
     def _get_hash_count(self, m, n) -> int:
